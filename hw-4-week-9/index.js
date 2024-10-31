1dbea9086c2eb035e4a5d1d019bda49e0c8daf11
Azamat <maratazamat09@gmail.com>
2024-11-01 02:10:07 +0500

    Изменил текст

---
+++ hw-4-week-9/index.js
@@ -0,0 +1,13 @@
+let algashky = Number(prompt('Бырыншы сан'))
+let ekinshi = Number(prompt('Екыншы сан сан'))
+let ushinshi = Number(prompt('Ушыншы сан'))
+
+let zhauab = (algashky + ekinshi + ushinshi) / 3
+
+alert('Енгізілген сандардың орташа арифметикалық мәні: ' + zhauab) 
+
+
+
+
+
+
