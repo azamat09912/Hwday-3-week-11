d26e67b0c3618307326cba9aeb7852e309cbcad9
Azamat <maratazamat09@gmail.com>
2024-11-18 21:29:15 +0500

    hw

---
+++ hw-week-12-day1/index.js
@@ -0,0 +1,9 @@
+function word(soz1, soz2) {
+    return soz1.toLowerCase() == soz2.toLowerCase() ? "Sozder birdei" : "sozder arturli";
+}
+
+
+let soz1 = prompt("Birinshi soz");
+let soz2 = prompt("ekinshi soz");
+alert(word(soz1, soz2));
+
